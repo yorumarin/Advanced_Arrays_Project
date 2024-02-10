f46d6a79691d926c9d6391df871c45c5a90b8623
Omega Dziko F. <megaoy@yahoo.com>
2024-02-10 17:15:09 -0500

    trippler prob stop

--- problems/02-tripler-map.js
+++ problems/02-tripler-map.js
@@ -5,14 +5,20 @@ array.
 
 Solve this using Array's `map()` method.
 
-Examples: 
+Examples:
 
 console.log(tripler([2, 7, 4])); // [ 6, 21, 12 ]
 console.log(tripler([-5, 10, 0, 11])); // [ -15, 30, 0, 33 ]
 */
 
 let tripler = function(nums) {
-    // Your code here 
+
+  
+    nums.map(num => {
+
+     num *= 3;
+    })
+    return num;
 };
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
