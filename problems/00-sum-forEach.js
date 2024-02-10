adfbd121e12c8c5c31ff1e549aa577d95bbc33ad
Omega Dziko F. <megaoy@yahoo.com>
2024-02-10 15:58:48 -0500

    test commit

--- problems/00-sum-forEach.js
+++ problems/00-sum-forEach.js
@@ -12,7 +12,8 @@ console.log(sumForEach([])); // 0
 */
 
 let sumForEach = function(arr) {
-  // Your code here 
+
+   d
 };
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
